f3064806e4960a59ba5f367a2cb8f6e0d21b5665
Emanuel <manu.brebal@gmail.com>
2025-11-10 09:40:35 -0300

    feat: Create Social Area views

---
+++ cancer-health-care-app/src/app/SocialArea/Forum/Chats/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Chats() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Visitar Áreas de Conversas</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#89e4ffff',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+});
---
+++ cancer-health-care-app/src/app/SocialArea/Forum/Topics/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Topics() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Visitar Tópicos da Comunidade</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#89e4ffff',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+});
