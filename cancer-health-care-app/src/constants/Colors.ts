6c2dcd77f44a0ab16a1b10cc8a0277e303f1d957
Emanuel <manu.brebal@gmail.com>
2025-12-29 20:39:46 -0300

    refactor: Reoganized global colors

--- cancer-health-care-app/src/constants/Colors.ts
+++ cancer-health-care-app/src/constants/Colors.ts
@@ -1,18 +1,31 @@
 export const Colors = {
     purplePrimary: '#9B5DE0',
     purpleSecondary: '#4E56C0',
+    lilacPrimary: '#D78FEE',
+
     background: '#FFFFFF',
+
+    tabBar: {
+        ActiveTintColor: '#8c4ad8ff',
+        InactiveTintColor: '#b15cceff',
+        backgroundColor: '#ffffff56',
+        borderColor: '#9B5DE0'
+    },
+
+
     text: {
         primary: '#1A1A1A',
         secondary: '#555555',
         white: 'white'
     },
+
     status: {
         success: 'green',
         error: 'red',
     },
+
     bgGradient: {
         start: 'white',
     },
-    lilacPrimary: '#D78FEE',
+
 }
