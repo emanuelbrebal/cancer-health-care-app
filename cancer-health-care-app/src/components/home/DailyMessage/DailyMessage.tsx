9702630b6b88137cc22768f593f7e5b8a3ffe33b
Emanuel <manu.brebal@gmail.com>
2026-01-12 21:44:56 -0300

    refactor: Some changes to DailyMessage component

--- cancer-health-care-app/src/components/home/DailyMessage/DailyMessage.tsx
+++ cancer-health-care-app/src/components/home/DailyMessage/DailyMessage.tsx
@@ -2,16 +2,17 @@ import { Image, Text, View } from 'react-native';
 import { DailyMessageStyles } from './DailyMessage.style';
 
 interface DailyMessageProps {
+  title?: string
   message: string
 }
 
-export default function DailyMessage({ message }: DailyMessageProps) {
+export default function DailyMessage({ title, message }: DailyMessageProps) {
   return (
     <View style={DailyMessageStyles.container}>
 
       <View style={DailyMessageStyles.helloMessageContainer}>
-        <Text style={DailyMessageStyles.text}>Mensagem do dia!</Text>
-        <Text style={DailyMessageStyles.subtext}>{message ? message : 'Mensagem do dia!'}</Text>
+        <Text style={DailyMessageStyles.text}>{title ? title : "Mensagem do dia!"}</Text>
+        <Text style={DailyMessageStyles.subtext}>{message ? message : "Mensagem do dia!"}</Text>
       </View>
 
       <View style={DailyMessageStyles.imageContainer}>
