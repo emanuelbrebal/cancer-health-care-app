42ebd6aaebf92a5890eb8a84ac75b1ebb8801605
Emanuel <manu.brebal@gmail.com>
2026-03-10 09:50:02 -0300

    feat (include modules): Included BooksModule to app.module and leisures.module

--- apps/backend/src/leisures/leisures.module.ts
+++ apps/backend/src/leisures/leisures.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LeisuresService } from './leisures.service';
 import { LeisuresController } from './leisures.controller';
-import { BooksModule } from 'src/books/books.module';
+import { BooksModule } from 'src/media/books/books.module';
 
 @Module({
   imports: [BooksModule],
