75036e99aeff204491639ee4e1f17fe0329f4ac8
Emanuel <manu.brebal@gmail.com>
2026-03-10 09:42:29 -0300

    remove: Rearrange files in "media"  folder for better organization

--- apps/backend/src/books/books.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BooksRepository } from './books.repository';
-import { BooksController } from './books.controller';
-
-@Module({
-  controllers: [BooksController],
-  providers: [BooksRepository],
-})
-export class BooksModule {}
