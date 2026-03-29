3d6414bb10c155a5bb612914305cb60a5e99f4ad
Emanuel <manu.brebal@gmail.com>
2026-03-29 09:49:40 -0300

    refactor (lorem text to the spiritual area): Added some placeholder text to the spiritual area

--- apps/mobile/src/app/(Home)/Oncology/SpiritualArea/index.tsx
+++ apps/mobile/src/app/(Home)/Oncology/SpiritualArea/index.tsx
@@ -1,6 +1,5 @@
 import { HorizontalBanner } from '@/src/components/ui/Images/HorizontalBanner';
 import { SearchBar } from '@/src/components/ui/Inputs/SearchBar';
-import RecomendationPager from '@/src/components/ui/Pagers/RecomendationPager';
 import { globalStyles } from '@/src/styles/global';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
@@ -14,10 +13,12 @@ export default function SpiritualArea() {
                 <View style={globalStyles.startContainer}>
                     <Text style={globalStyles.title}>Cuidados com o sono: </Text>
                     <Text style={[globalStyles.title, { fontSize: 12 }]}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam nemo velit totam nostrum iure quod nihil rem vitae in ratione perferendis, hic doloremque praesentium magnam reprehenderit amet veniam ex! </Text>
-                </View> <View style={globalStyles.startContainer}>
+                </View> 
+                <View style={globalStyles.startContainer}>
                     <Text style={globalStyles.title}>Cuidados com o sono: </Text>
                     <Text style={[globalStyles.title, { fontSize: 12 }]}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam nemo velit totam nostrum iure quod nihil rem vitae in ratione perferendis, hic doloremque praesentium magnam reprehenderit amet veniam ex! </Text>
-                </View> <View style={globalStyles.startContainer}>
+                </View> 
+                <View style={globalStyles.startContainer}>
                     <Text style={globalStyles.title}>Cuidados com o sono: </Text>
                     <Text style={[globalStyles.title, { fontSize: 12 }]}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ipsam nemo velit totam nostrum iure quod nihil rem vitae in ratione perferendis, hic doloremque praesentium magnam reprehenderit amet veniam ex! </Text>
                 </View>
