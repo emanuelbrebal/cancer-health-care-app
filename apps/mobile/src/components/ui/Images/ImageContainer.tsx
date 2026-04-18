e2644dd176048aeeecdbc715d80004fc7fa7692d
Emanuel <manu.brebal@gmail.com>
2026-04-18 09:33:37 -0300

    ♻️ refactor(components): fix ImageContainer reverted prop

--- apps/mobile/src/components/ui/Images/ImageContainer.tsx
+++ apps/mobile/src/components/ui/Images/ImageContainer.tsx
@@ -3,9 +3,16 @@ import { globalStyles } from "@/src/styles/global";
 import { Image, View } from "react-native";
 
 export function ImageContainer({ imagePath, reverted = false }: imageContainerProps) {
-    return (
-        <View style={globalStyles.imageContainer}>
-            {imagePath && <Image source={imagePath || require('@assets/images/Home/blueMascotPlaceholder.png')} style={reverted? globalStyles.bigImage : globalStyles.bigImageReverted} resizeMode="contain"/>}
-        </View>
-    )
+  return (
+    <View style={globalStyles.imageContainer}>
+      <Image
+        source={imagePath || require('@assets/images/Home/blueMascotPlaceholder.png')}
+        style={[
+          globalStyles.bigImage,
+          reverted && { transform: [{ scaleX: -1 }] } 
+        ]}
+        resizeMode="contain"
+      />
+    </View>
+  );
 }
